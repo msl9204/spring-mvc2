d52bcc7358d23074c4477552caf08f7639329399
minsoo.lee <letomok@gmail.com>
2021-12-06 23:04:13 +0900

    챕터 8부터commit

---
+++ login-start/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,39 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void createSession() {
+
+        // 세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        // 세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        // 세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+    }
+}
