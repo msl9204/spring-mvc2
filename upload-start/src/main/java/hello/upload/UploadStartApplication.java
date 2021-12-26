140667471e1d0f148e7f2c817935626dee2c043c
minsoo.lee <letomok@gmail.com>
2021-12-26 21:08:57 +0900

    파일 업로드 - 스프링

---
+++ upload-start/src/main/java/hello/upload/UploadStartApplication.java
@@ -0,0 +1,13 @@
+package hello.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadStartApplication.class, args);
+    }
+
+}
